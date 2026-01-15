cfbac92c6b9e11cadc8fc11a596ad969224308cd
danielamorenodesign <danielamoreno.design@gmail.com>
2026-01-14 22:48:56 -0600

    flecha scroll mobile  script.js

--- script.js
+++ script.js
@@ -424,3 +424,21 @@ function updateDots() {
 
 
 /* carrusel de imagenes */
+
+
+const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+      scrollTopBtn.style.display = "flex";
+    } else {
+      scrollTopBtn.style.display = "none";
+    }
+  });
+
+  scrollTopBtn.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
